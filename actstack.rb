0588260cb09a50fb4baf14f521fdc2dfa29ce114
Chad Jemmett <chad.jemmett@gmail.com>
2015-07-07 22:48:53 -0600

    add rspec stuff

--- actstack.rb
+++ actstack.rb
@@ -2,42 +2,49 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './actstack_model'
 
-put "/:id/:vote" do
-  
+
+#upvote premise
+put "/premise/:id/:vote" do
   @premise = Premise.get(params[:id])
   @premise.vote = @premise.vote.to_i + params[:vote].to_i
   @premise.save
   redirect "/"
 end
 
+#show index and all premise 
 get "/" do
   @list = Premise.all
   erb :index
 end
 
+#new premise post request
 post "/new_premise" do
   n = Premise.new
   #need to add save at time and other data.
   n.premise = params[:premise]
   n.save
-  redirect "/#{n.id}"
+  redirect "/premise/#{n.id}"
 end
 
+#readme page
 get "/ideas" do
   markdown :README
 end
 
-get "/:id" do
+#show individual premise
+get "/premise/:id" do
   @premise = Premise.get params[:id]
   erb :show
 end
 
-get "/:id/acts/new" do
+#new act form
+get "/premise/:id/acts/new" do
   @premise = Premise.get(params[:id])
   puts @premise.id
   erb :new_act
 end
 
+#new act post request
 post "/:id/acts" do
   @premise = Premise.get(params[:id])
   @params = params
@@ -47,9 +54,3 @@ post "/:id/acts" do
   act.save
   redirect "/#{@params[:id]}"
 end
-
-  
-get "/test_page" do
-  erb :test_page
-end
-
